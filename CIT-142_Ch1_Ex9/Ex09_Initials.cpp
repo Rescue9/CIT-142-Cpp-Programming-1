c16eaa921578fe03bbc026596a783b534f15a5c1
Rescue9 <Rescue@corridor9.net>
2013-08-22 18:45:00 -0500

    Ch1 Exercises finished

---
+++ CIT-142_Ch1_Ex9/Ex09_Initials.cpp
@@ -0,0 +1,22 @@
+/*
+ * Program: Initials.cpp
+ * Programmer: Andrew Buskov
+ * Class: CIT 142
+ * Date: Aug 22, 2013
+ */
+
+/* Purpose: To create my initials 5 lines high, using my initials */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	// no classes this time.. just simple output
+	cout << "    A       BBBBB  \n";
+	cout << "   A A      B    B \n";
+	cout << "  AAAAA     BBBBB  \n";
+	cout << " A     A    B    BB\n";
+	cout << "A       A   BBBBB  \n";
+
+	return 0;
+}
