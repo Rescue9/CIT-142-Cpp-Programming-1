bf3573a4c397bc78cfd40f80219e6e088f63dce8
Rescue9 <Rescue@corridor9.net>
2013-08-29 14:17:23 -0500

    completed Ch2 exercised 2 & 4

---
+++ CIT-142_Testbed/Employee.cpp
@@ -0,0 +1,28 @@
+/*
+ * Program: Employee.cpp
+ * Programmer: Andrew Buskov
+ * Class: CIT 142
+ * Date: Aug 29, 2013
+ */
+
+// TESTING
+
+#include <iostream>
+using namespace std;
+
+struct Employee {
+	int idNumber;
+	double hourlySalary;
+};
+
+int main() {
+	int a = 2;
+	int c = a++;
+
+	cout << "a is " << a << " and c is " << c << endl;
+	return 0;
+}
+
+
+
+
