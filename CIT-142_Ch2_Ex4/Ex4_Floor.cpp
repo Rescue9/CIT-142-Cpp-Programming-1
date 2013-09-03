6bcdc2e843ec0412ea14089b3ec14cb3ce1c8799
Rescue9 <Rescue@corridor9.net>
2013-09-02 20:46:00 -0500

    corrected errors in exercise 4

--- CIT-142_Ch2_Ex4/Ex4_Floor.cpp
+++ CIT-142_Ch2_Ex4/Ex4_Floor.cpp
@@ -10,28 +10,34 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
 
 	// declare variables
-	double installationHours;
+	int installationHours;
 	int installationDays;
+	int remainingInstallationHours;
 	const int RATE_PER_DAY = 200;
 	const int RATE_PER_HOUR = 40;
 	double totalCost;
 
 	// get user input
-	cout << "How many complete days will it take to install flooring? " << endl;
-	cin >> installationDays;
-	cout << "How many additional hours will be needed on the last day? " << endl;
+	cout << "How many complete hours will it take to install flooring? "
+			<< endl;
 	cin >> installationHours;
 
-	// calculate & display
-	totalCost = (installationDays * RATE_PER_DAY) + (installationHours * RATE_PER_HOUR);
-	cout << "The total estimated cost to install said flooring is " << totalCost << "." << endl;;
+	// calculate time
+	installationDays = installationHours / 8;
+	remainingInstallationHours = installationHours % 8;
+
+	// calculate price & display
+	totalCost = (installationDays * RATE_PER_DAY)
+			+ (remainingInstallationHours * RATE_PER_HOUR);
+	cout << "The total estimated cost to install said flooring is " << totalCost
+			<< "." << endl;;
+	cout << "This is calculated a $200/day (" << installationDays
+			<< ") and $40/additional hour (" << remainingInstallationHours
+			<< ")." << endl;
 
 	return 0;
 }
-
-
-
 
