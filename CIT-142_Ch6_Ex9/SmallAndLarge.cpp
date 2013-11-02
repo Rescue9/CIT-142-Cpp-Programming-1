16d21597c50524a1048739d4df4e777b0d2be49b
Rescue9 <Rescue@corridor9.net>
2013-11-01 20:08:10 -0500

    finished Ch6_Ex9

--- CIT-142_Ch6_Ex9/SmallAndLarge.cpp
+++ CIT-142_Ch6_Ex9/SmallAndLarge.cpp
@@ -14,3 +14,29 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+	// TODO: Change debug to false for production
+	const bool DEBUG = false;
+	cout << "Please enter 10 separate values:";
+	const int SIZE = 10;
+	double values[SIZE];
+	for (int i=0; i<SIZE; i++){
+		cout << "Value: ";
+		cin >> values[i];
+	}
+
+	//TODO: used for testing
+	if (DEBUG) {
+		for (int i = 0; i < 10; i++) {
+			cout << values[i] << endl;
+		}
+	}
+
+	double findSmallest(double[], int);
+	double findLargest(double[], int);
+
+
+	cout << "The smallest number is: " << findSmallest(values, SIZE) << endl;
+	cout << "The largest number is: " << findLargest(values, SIZE) << endl;
+
+}
