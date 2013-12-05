d50330e0731f1a1a30fd1f43a272034c87fec29b
Rescue9 <Rescue@corridor9.net>
2013-12-05 14:22:25 -0600

    began final project.

---
+++ CIT-142_Final/CIS-142_Final.Hangman.cpp
@@ -0,0 +1,34 @@
+/*
+ * Program: CIS-142_Final.Hangman.cpp
+ * Programmer: Andrew Buskov
+ * Class: CIT 142
+ * Date: Dec 5, 2013
+ */
+
+/* Purpose: Create the video game hangman in the command prompt while meeting the following requirements:
+ Require the user to input letters only. Allow upper or lower case letters.
+ Randomly select between at least 5 words for the user to guess. Keep a theme (e.g. animals, foods, philosophers, etc).
+ All words should be at least 5 characters.
+ Display a message when the user incorrectly guesses a letter.
+ Display a message when the user correctly guesses a letter.
+ Display all previously guessed letters so the user doesn't guess them again.
+ Allow the user to incorrectly answer 6 times before displaying a game over message. (head, body, arms, legs)
+ Display a congratulations message if the user wins.
+ Prompt the user to play again once their game is over.
+ Display correctly guessed letters with Xs in place of the unguessed letters (e.g. ProXraXXinX = Programming with G and M unguessed)
+ You don't have to draw the actual hanging man.
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+string pickRandomWord(){
+
+}
+
+
+
+int main (){
+
+}
